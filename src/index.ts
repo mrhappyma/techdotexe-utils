30c409497430580e7d290cfc2967bf0ec849cd35
Dominic Ruggiero <dominic@userexe.me>
2023-07-05 17:44:15 -0400

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import { Client } from "discord.js";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const bot = new Client({ intents: ["Guilds", "GuildMessages"] });
+
+bot.on("ready", () => {
+  console.log("Client is ready!");
+});
+
+bot.on("messageCreate", async (message) => {
+  if (message.content.startsWith("set channel name to")) {
+    if (message.channel.isDMBased()) return;
+    message.channel.setName(
+      message.content.replace("set channel name to ", "")
+    );
+  }
+});
+
+bot.login(process.env.DSC_TOKEN);
